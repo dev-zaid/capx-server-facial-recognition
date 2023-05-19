8eeea531c72ed5b6e745516453d8fe6bec66c385
YARE0909 <pradyumnad092004@gmail.com>
2023-05-19 23:58:07 +0530

    ✨ Added API route

--- src/api/uniqueness/router.ts
+++ src/api/uniqueness/router.ts
@@ -1,8 +1,25 @@
 import { Request, Response, Router } from "express";
+import AI from "../../config/AI";
 
 const uniquenessRouter = Router();
 
-const handleUniqueness = (req: Request, res: Response) => {};
+const headers = {
+  "X-API-Key": process.env.API_KEY,
+};
 
-uniquenessRouter.get("/", handleUniqueness);
+const handleUniqueness = async(req: Request, res: Response) => {
+  const { image } = req.body;
+  const body = {
+    collection_id: "68110664-ab19-4227-9d35-961197efc88d",
+    images: [image],
+    min_score: 0.7,
+    search_mode: "FAST",
+  };
+  const response = await AI.post("/search", body, { headers });
+  if (!response) {
+    return res.status(200).send("Image is unique!");
+  }
+};
+
+uniquenessRouter.post("/uniqueness", handleUniqueness);
 export default uniquenessRouter;
